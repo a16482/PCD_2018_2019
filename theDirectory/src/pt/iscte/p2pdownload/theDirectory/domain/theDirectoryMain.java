99f928fc7f4470afb2ef9045b0ce0670612426b2
a16482-iscteiulpt <a16482@iscte-iul.pt>
2018-10-27 10:34:09 +0100

    more

---
+++ theDirectory/src/pt/iscte/p2pdownload/theDirectory/domain/theDirectoryMain.java
@@ -0,0 +1,16 @@
+package pt.iscte.p2pdownload.theDirectory.domain;
+
+public class theDirectoryMain {
+
+	public static void main(String[] args) {
+		// TODO check argumentos
+		
+		//lançar um  servidor
+		
+		// ficar à espera de pedidos
+		
+		//à medida que chegam os pedidos, tem que estabelecer uma socket
+		
+	}
+
+}
