214e104bded6659c1fa7a9ab0cbe4f8bbee25ccf
a16482-iscteiulpt <a16482@iscte-iul.pt>
2018-10-29 23:14:51 +0000

    Update theISCTEbay/src/pt/iscte/p2pdownload/theISCTEbay/domain/InterfaceGrafica.java

--- theISCTEbay/src/pt/iscte/p2pdownload/theISCTEbay/domain/InterfaceGrafica.java
+++ theISCTEbay/src/pt/iscte/p2pdownload/theISCTEbay/domain/InterfaceGrafica.java
@@ -1,20 +1,48 @@
 package pt.iscte.p2pdownload.theISCTEbay.domain;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.beans.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingWorker;
 
 public class InterfaceGrafica extends JPanel implements ActionListener, PropertyChangeListener {
 	private static final long serialVersionUID = 1L;
 	
+	private static int W = 800 ;
+	private static int H = 600 ;
+	
 	private JProgressBar barraDeProgresso;
     private JButton botaoDescarregar;
+    private JButton botaoProcurar;
     private JTextArea outputTarefa;
     private Task tarefa;
     private JLabel lblTexto;
+    private JLabel lblTitulo;
     private JTextField txtField;
+    private JList<String> listaFiles;
  
     class Task extends SwingWorker<Void, Void> {
         @Override
@@ -54,13 +82,12 @@ public class InterfaceGrafica extends JPanel implements ActionListener, Property
  
     public InterfaceGrafica() {
        	//---------------------------------------------------
-       	//Experiência:
+        //Criação da Interface com o utilizador (GUI)
        	//---------------------------------------------------
     	
-        //Criação da Interface com o utilizador (GUI).
         super(new BorderLayout());
              
-        lblTexto = new JLabel("Texto a procurar:");
+        lblTexto = new JLabel("Texto a procurar: ");
 		Font fontLblTexto = new Font("LucidaSans", Font.PLAIN, 14);
 		lblTexto.setFont(fontLblTexto);
 		lblTexto.setForeground(Color.red);
@@ -69,16 +96,50 @@ public class InterfaceGrafica extends JPanel implements ActionListener, Property
 		lblTexto.setHorizontalAlignment(JTextField.LEFT);
 		
 		txtField = new JTextField();
-		txtField.setText("");
+		txtField.setText("Insira aqui um argumento de pesquisa");
+		txtField.setPreferredSize(size());
 		txtField.setFont(fontLblTexto);
 		txtField.setHorizontalAlignment(JTextField.LEFT);
 		txtField.setSelectedTextColor(Color.BLUE);
+		
+		botaoProcurar = new JButton("Procurar");
+		botaoProcurar.setActionCommand("Procurar");
+		botaoProcurar.addActionListener(this);
+	 
 		  
 		JPanel painelProcura= new JPanel();
-		painelProcura.setLayout(new GridLayout(1, 2));
+		painelProcura.setLayout(new GridLayout(1, 3));
 		painelProcura.add(lblTexto);
 		painelProcura.add(txtField);
-  
+		painelProcura.add(botaoProcurar);
+		
+		//Criação dos elementos da GUI relacionados com a lista de ficheiros
+		//Titulo
+		lblTitulo = new JLabel("Titulo");
+		Font fontLblTitulo = new Font("LucidaSans", Font.PLAIN, 14);
+		lblTitulo.setFont(fontLblTitulo);
+		lblTitulo.setForeground(Color.red);
+		lblTitulo.setBackground(Color.gray);
+		lblTitulo.setVerticalAlignment(JTextField.CENTER);
+		lblTitulo.setHorizontalAlignment(JTextField.LEFT);
+		
+		// Cria uma JList que mostra strings de um array
+		String[] searchResult = null; // Array de Strings
+		//JList<String> listaFiles = new JList<String>(searchResult);
+		listaFiles = new JList<String>(searchResult);
+		listaFiles.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		// 3 hipóteses: a)SINGLE_SELECTION; b) SINGLE_INTERVAL_SELECTION; c) MULTIPLE_INTERVAL_SELECTION
+		listaFiles.setLayoutOrientation(JList.VERTICAL);
+		listaFiles.setVisibleRowCount(-1); 		
+//		...
+//		JScrollPane listScroller = new JScrollPane(list);
+//		listScroller.setPreferredSize(new Dimension(250, 80));
+//		...
+		JPanel painelFicheiros = new JPanel();
+		painelFicheiros.setLayout(new GridLayout(2, 1));
+		painelFicheiros.add(lblTitulo);		
+		painelFicheiros.add(listaFiles);
+        
         //Criação dos elementos da GUI relacionados com a barra de progresso..
         botaoDescarregar = new JButton("Descarregar");
         botaoDescarregar.setActionCommand("Descarregar");
@@ -101,6 +162,7 @@ public class InterfaceGrafica extends JPanel implements ActionListener, Property
         setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
  
         add(painelProcura, BorderLayout.NORTH);
+//        add(painelFicheiros, BorderLayout.EAST);
         add(painelProgresso, BorderLayout.SOUTH);
         
         //add(painel1, BorderLayout.PAGE_START);
@@ -109,9 +171,23 @@ public class InterfaceGrafica extends JPanel implements ActionListener, Property
  
     }
  
-     //----------------------------------------------------------
-     // Invocado quando o utilizador carrega no botão "Descarregar".
-     //---------------------------------------------------------------------
+//    public void valueChanged(ListSelectionEvent e) {
+//        if (e.getValueIsAdjusting() == false) {
+//
+//            if (listaFiles.getSelectedIndex() == -1) {
+//            //Nada selecionado, disable botão de fire
+//                fireButton.setEnabled(false);
+//
+//            } else {
+//            //Selecionado, enable do botao fire.
+//                fireButton.setEnabled(true);
+//            }
+//        }
+//    }
+    
+    //----------------------------------------------------------
+    // Invocado quando o utilizador prime o botão "Descarregar".
+    //---------------------------------------------------------------------
     public void actionPerformed(ActionEvent evt) {
         botaoDescarregar.setEnabled(false);
         setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
@@ -142,7 +218,17 @@ public class InterfaceGrafica extends JPanel implements ActionListener, Property
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("The ISCTE Bay");
+        frame.setSize ( H , W ) ; // Isto não está a funcionar!
+        frame.setAlwaysOnTop(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+//        JFrame frame = new JFrame();
+//        frame.setTitle ("The ISCTE Bay") ;
+//        frame.setSize ( H , W ) ;
+//        //frame.setLayout(new FlowLayout());
+//        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+//        //createContent();
+//        //frame.setVisible(true);
  
         //Cria e configura o painael de conteúdos.
         JComponent newContentPane = new InterfaceGrafica();
