26f6a6941669bb8a7149cc6ca4bc0c378680ca05
a16482-iscteiulpt <a16482@iscte-iul.pt>
2018-11-01 12:30:27 +0000

    comit1

--- P2PDowload/TheISCTEBay/src/pt/iscte/P2PDownload/TheISCTEBay/IGDownload.java
+++ P2PDowload/TheISCTEBay/src/pt/iscte/P2PDownload/TheISCTEBay/IGDownload.java
@@ -2,120 +2,65 @@ package pt.iscte.P2PDownload.TheISCTEBay;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.*;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
-import javax.swing.SwingWorker;
 
-public class TheISCTEBay extends JPanel implements ActionListener, PropertyChangeListener {
+import pt.iscte.P2PDownload.TheISCTEBay.TheISCTEBay.Task;
+
+//import pt.iscte.P2PDownload.TheISCTEBay.TheISCTEBay.Task;
+
+//public class IGCliente extends JFrame {
+public class IGDownload extends JPanel implements ActionListener, PropertyChangeListener {
+
+	super(new BorderLayout());
+	
 	private static final long serialVersionUID = 1L;
-
-
-	private static String enderecoDiretorio;
-	private static int portoDiretorio;
-	private static int portoUtilizador;
-	private static String pastaTransferencias;
-	private static Diretorio d;
+	
 	//--------------------------------------------	
-	// Variaveis e Constantes da IG
+	// Variaveis e Constantes da IGDownload
 	//--------------------------------------------
 	private static int W = 800;
 	private static int H = 400;
 	private JProgressBar barraDeProgresso;
 	private JButton botaoDescarregar;
 	private JButton botaoProcurar;
-	// private JTextArea tarefaOutput;
-	private Task tarefa;
 	private JLabel lblTexto;
 	private JTextField txtField;
 	private JList<String> listaFiles;
 	private JScrollPane listScroller;
+	private JTextArea tarefaOutput;
+	private Task tarefa;
 	//--------------------------------------------
 	
-	//-------------------------------------------------------------------------------------------------------
-	//-------------------------------------------------------------------------------------------------------
-
-    class Task extends SwingWorker<Void, Void> {
-        //private Object icon;
-
-    	@Override
-    	public Void doInBackground() {
-    		//--------------------------------------
-    		Random random = new Random();  //--> ALTERAR!
-    		//--------------------------------------
-
-    		int progresso = 0;
-    		//Inicializa a propriedade "progress".
-    		setProgress(0);
-    		while (progresso < 100) {
-    			//Sleep até 1 segundo.
-    			try {
-    				Thread.sleep(random.nextInt(1000));
-    			} catch (InterruptedException ignore) {}
-    			//--> ALTERAR ISTO!
-    			//Tornar o progresso random.
-    			progresso += random.nextInt(10);
-    			setProgress(Math.min(progresso, 100));
-    		}
-    		return null;
-    	}
-              
-    	//---------------------------------------------------
-        // Executado no evento de despachar a thread
-     	//---------------------------------------------------
-    	@Override
-    	public void done() { //Feito!!!
-    		Toolkit.getDefaultToolkit().beep();
-    		botaoDescarregar.setEnabled(true);
-    		botaoProcurar.setEnabled(true);
-    		txtField.setEnabled(true);
-    		listaFiles.setEnabled(true);
-    		setCursor(null); //desliga o wait do cursor
-    		// Mostra o que foi feito ao Cliente:
-    		//ImageIcon icon = createImageIcon("images/middle.gif","this is a caption");
-
-    		//-----------------------------------------------------         
-    		//Recuperar as linhas seguintes para a mensagem final!   
-    		//Formato da msg final: Fornecedor{endereço=ip, porto=p]: blocos
-    		//Exemplo:
-    		//(ícone I) Descarga completa.
-    		//Fornecedor[endereço=/127.0.0.1, porto=8082]:253 
-    		//Fornecedor[endereço=/127.0.0.1, porto=8081]:251 
-    		//-----------------------------------------------------
-//    		 ImageIcon icon = createImageIcon("images/middle.gif","this is a caption");
-    		//ImageIcon iconeInfo = createImageIcon("../Icon/iconInfo.png", "Informação");
-    		 MsgBox.show("YOUR INFORMATION HERE", "TITLE BAR MESSAGE", 0);    
-    	}
-    }
-
-
-    public TheISCTEBay() {
-       	//---------------------------------------------------
-        //Criação da Interface com o utilizador (GUI)
-       	//---------------------------------------------------
-        super(new BorderLayout());
-        //Painel suoerior     
-        lblTexto = new JLabel("Texto a procurar: ");
+	
+	class IGDownload() {
+		//---------------------------------------------------
+		//Criação da Interface com o utilizador (GUI)
+		//---------------------------------------------------
+	
+		
+		//Painel suoerior     
+		lblTexto = new JLabel("Texto a procurar: ");
 		Font fontLblTexto = new Font("Lucida Sans Serif", Font.BOLD, 16);
 		lblTexto.setFont(fontLblTexto);
 		lblTexto.setForeground(Color.DARK_GRAY);
@@ -135,8 +80,9 @@ public class TheISCTEBay extends JPanel implements ActionListener, PropertyChang
 		botaoProcurar.setFont(fontBotaoProcurar);
 		botaoProcurar.setActionCommand("Procurar");
 		botaoProcurar.setPreferredSize(new Dimension(W / 8 * 3, H / 10));
-		botaoProcurar.addActionListener(this);
- 
+		botaoProcurar.addActionListener((ActionListener) this);
+		//botaoProcurar.addActionListener(this);
+
 		JPanel painelProcura = new JPanel();
 		painelProcura.setLayout(new BorderLayout());
 		painelProcura.add(lblTexto, BorderLayout.WEST);
@@ -186,7 +132,7 @@ public class TheISCTEBay extends JPanel implements ActionListener, PropertyChang
 		listaFiles.setFont(new Font("Lucida Sans Serif", Font.PLAIN, 16));
 		listScroller.setViewportView(listaFiles);
 		listScroller.setPreferredSize(new Dimension (W/8*3, H/3*2-10));
- 
+
 		JPanel painelProgresso = new JPanel();
 		painelProgresso.setLayout(new GridLayout(2, 1));
 		painelProgresso.add(botaoDescarregar);
@@ -196,12 +142,11 @@ public class TheISCTEBay extends JPanel implements ActionListener, PropertyChang
 		add(painelProcura, BorderLayout.NORTH);
 		add(painelFicheiros, BorderLayout.WEST);
 		add(painelProgresso, BorderLayout.EAST);
-       	//---------------------------------------------------
-        //Fim do GUI ----------------------------------------
-       	//---------------------------------------------------
+		//---------------------------------------------------
+		//Fim do GUI ----------------------------------------
+		//---------------------------------------------------
 	}
-    
-    // ------------------------------------------------------------------------
+ 	// ------------------------------------------------------------------------
 	// Invocado quando o utilizador prime o botão "Descarregar" ou "Procurar".
 	// ------------------------------------------------------------------------
 	@Override
@@ -222,7 +167,7 @@ public class TheISCTEBay extends JPanel implements ActionListener, PropertyChang
 
 		}
 	}
-
+	
 	// ------------------------------------------------------------------------
 	// Invocado sempre que a propriedade de progresso da tarefa é alterada
 	// ------------------------------------------------------------------------
@@ -232,69 +177,6 @@ public class TheISCTEBay extends JPanel implements ActionListener, PropertyChang
 			int progress = (Integer) evt.getNewValue();
 			barraDeProgresso.setValue(progress);
 		}
-	}
-
-	// ------------------------------------------------------------------------
-	// Criação do GUI e colocação em funcionamento.
-	// Corre no evento que despacha a thread.
-	// ------------------------------------------------------------------------
-	private static void criaEmostraGUI() {
-		JFrame frame = new JFrame("The ISCTE Bay");
-		frame.setPreferredSize(new Dimension(W, H)); // Isto não está a funcionar!
-		frame.setAlwaysOnTop(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Cria e configura o painel de conteúdos.
-		JComponent newContentPane = new TheISCTEBay();
-		newContentPane.setOpaque(true); // Os painéis de conteúdos devem ser opacos !!!
-		frame.setContentPane(newContentPane);
-
-		// Mostra a janela.
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		//Verifica se a aplicação iniciou com os 4 argumentos:<IP do Diretório> <Porto do Diretório> <Porto do Utilizador> <Pasta para transferências>
-		if (args.length < 4) {
-			System.err.println(
-					"São necessários 4 argumentos: <IP do Diretório> <Porto do Diretório> <Porto do Utilizador> "
-							+ "<Pasta para transferências>");
-			System.exit(1);
-		}
-		
-		//Guarda os argumentos em variáveis da classe
-		enderecoDiretorio = args[0];
-		portoDiretorio = Integer.parseInt(args[1]);
-		portoUtilizador = Integer.parseInt(args[2]);
-		pastaTransferencias = args[3];
-		
-		//Mostra os arumentos recebidos na consola
-		System.out.println("IP do Diretório: " + enderecoDiretorio + "\nPorto do Diretorio: " + portoDiretorio
-				+ "\nPorto do Utilizador: " + portoUtilizador + "\nPasta para transferências: " + pastaTransferencias);
-		
-		//Instancia um diretório
-		d = new Diretorio (enderecoDiretorio, portoDiretorio, portoUtilizador);
-		
-		//Regista-se no Diretório
-		d.registoDiretorio();
-		
-		//Consulta a lista de utilizadores no Diretório
-		d.consultaUtilizadores();
-		
-		List<Utilizador> listaUtilizadores = d.getListaUtilizadores();
-		
-		for (Utilizador u : listaUtilizadores) {
-			System.out.println(u.ipUtilizador() + " " + u.portoUtilizador());
-		}
-		
-		// Agenda um job para o evento de despachar a thread.
-		// Cria e mostra o GUI desta aplicação.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				criaEmostraGUI();
-			}
-		});
+	
 	}
 }
